3eaf1eac0d5bbed41e0de812ca8246ad60c73ad7
StudCV <60759153+StudCV@users.noreply.github.com>
2020-02-26 18:16:29 +0300

    new

--- lab2.py
+++ lab2.py
@@ -4,7 +4,6 @@ a,s,b = input("Введите операнды в формате 'первый �
 определяем: была ли введена константа
 '''
 def pars(x):
-<<<<<<< HEAD
     length=len(x)
     if length < 3:
         if x=="e":
@@ -14,17 +13,6 @@ def pars(x):
             from math import pi as x
             return x
     return float(x)
-=======
-    length=len(a)
-    if length < 3:
-        if a=="e":
-            from math import e as x
-            return x
-        elif a=="pi":
-            from math import pi as x
-            return x
-    return int(x)
->>>>>>> 28e98bdfe86148b628135cf7034729495b598817
 
 if s=="+":
     print(str(a)+"+"+str(b)+"="+str('%.2f' % (pars(a)+pars(b))))
