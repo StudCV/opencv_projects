70c8be70600f95e5e3a3a78fccf4008e6b44a930
StudCV <60759153+StudCV@users.noreply.github.com>
2020-02-27 22:06:34 +0300

    made on 27 february
    i am making the last two exercise

--- lab3.py
+++ lab3.py
@@ -1,19 +1,27 @@
-import cv2
+cv2.imshow('GRAYSCALE' , imggrey)import cv2
 import numpy as np
 img = cv2.imread('lab3.png', cv2.IMREAD_COLOR)
 imggrey = cv2.imread('lab3.png', cv2.IMREAD_GRAYSCALE)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+#1
+cv2.imshow('RGB' ,img)
+cv2.imshow('GRAYSCALE' , imggrey)
 cv2.imwrite('testg.jpg',imggrey)
-cv2.imwrite('test.jpg',img )
-cv2.imshow('inv',img[:,:,1])
 
-invert= np.copy(imggrey)
-for i in range(len(imggrey)):
-    imggrey[i] = abs(imggrey[i] - 255)
+#Для каждого пикселя инвертируйте его значение 
+height, width, _ = imggrey.shape
+for y in range(height):
+    for x in range(width):
+        imggrey[x][y] =  255 - imggrey[x][y]
 cv2.imwrite('testinv.jpg',imggrey)
 
-cv2.imshow('GRAYSCALE' , imggrey)
-cv2.imshow('NORM' ,img)
+#Поменяйте местами значения красного и зеленого каналов.
+for y in range(height):
+    for x in range(width):
+        imggrey[x][y][1],imggrey[x][y][2] =  imggrey[x][y][2],imggrey[x][y][1]
+cv2.imwrite('test3.jpg',imggrey)
+        
 
-cv2.waitKey(20000)
+
+
+cv2.waitKey(0)
