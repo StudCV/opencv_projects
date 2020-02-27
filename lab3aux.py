36d4eb28556b3119afdc68b0f3550b633493e377
StudCV <60759153+StudCV@users.noreply.github.com>
2020-02-27 22:23:41 +0300

    Update lab3aux.py

--- lab3aux.py
+++ lab3aux.py
@@ -3,12 +3,17 @@
 RFflag = np.empty((300, 200, 3))
 for y in range(300):
     for x in range(200):
-        for c in range(3):
             if y<100:
-                if c==0
-                  RFflag[y][x][c]
-cv2.imshow('RGB' ,RFflag)
+                RFflag[y][x] = (255,255,255)
+            elif y<200:
+                RFflag[y][x] = (255,0,0) 
+            else:
+                RFflag[y][x] = (0,0,255) 
+cv2.imshow('Rf flag' ,RFflag)
 
+#палитра
+cv2.namedWindow('Colorbars')
+#
 #cv2.imwrite('test.jpg',img )
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #cv2.imshow('inv',img[:,:,1])
