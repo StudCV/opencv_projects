a59c2b55b72ede3e87fc53971e182fbd9f5576eb
StudCV <60759153+StudCV@users.noreply.github.com>
2020-03-09 21:02:45 +0300

    Update BDT_Lab3.py

--- BDT_Lab3.py
+++ BDT_Lab3.py
@@ -7,5 +7,11 @@ cv2.imshow = ('RGB',imagecolour)
 cv2.imshow = ('GRAYSCALE',imagegrey)
 
 
-cv2.imwrite('/home/user/Documents/91-2/Materials_for_laba_3/kirbygrey.jpg',imagegrey)
-cv2.waitKey(-1)
+'''cv2.imwrite('/home/user/Documents/91-2/Materials_for_laba_3/kirbygrey.jpg',imagegrey)'
+'cv2.waitKey(-1)'''
+
+'''
+color_image_rgb = cv2.cvtColor(imagecolour, cv2.COLOR_BGR2RGB) %maybe from BGR2BRG?%
+cv2.imshow('Color RGB', color_image_rgb)
+cv2.waitKey(0)
+'''
