41bba57a57c46048fd673d9a86e7c7c2947c6422
StudCV <60759153+StudCV@users.noreply.github.com>
2020-03-10 14:37:41 +0300

    Update BDT_Lab3.py

--- BDT_Lab3.py
+++ BDT_Lab3.py
@@ -1,17 +1,22 @@
 import cv2
-import numpy as numpy
+import numpy as np
 
 imagecolour = cv2.imread('/home/user/Documents/91-2/Materials_for_laba_3/kirby.jpg',cv2.IMREAD_COLOR)
 imagegrey = cv2.imread('/home/user/Documents/91-2/Materials_for_laba_3/kirby.jpg',cv2.IMREAD_GRAYSCALE)
-cv2.imshow = ('RGB',imagecolour)
-cv2.imshow = ('GRAYSCALE',imagegrey)
+cv2.imshow ('RGB',imagecolour)
+cv2.imshow ('GRAYSCALE',imagegrey)
 
 
-'''cv2.imwrite('/home/user/Documents/91-2/Materials_for_laba_3/kirbygrey.jpg',imagegrey)'
-'cv2.waitKey(-1)'''
+cv2.imwrite('/home/user/Documents/91-2/Materials_for_laba_3/kirbygrey.jpg',imagegrey)
 
-'''
-color_image_rgb = cv2.cvtColor(imagecolour, cv2.COLOR_BGR2RGB) %maybe from BGR2BRG?%
-cv2.imshow('Color RGB', color_image_rgb)
-cv2.waitKey(0)
-'''
+invertkirby = np.copy(imagegrey)
+invertkirby = 255-invertkirby
+cv2.imshow ('GRAYSCALEINVERT',invertkirby)
+
+kibyrgb = cv2.imread('/home/user/Documents/91-2/Materials_for_laba_3/kirby.jpg',cv2.IMREAD_COLOR)
+(x,y,z)=kibyrgb.shape
+for i in range (0,x):
+    for j in range (0,y):
+        kibyrgb [i][j][1], kibyrgb [i][j][2]= kibyrgb [i][j][2], kibyrgb [i][j][1]
+cv2.imshow ('rgbinvert',kibyrgb)
+cv2.waitKey(-1)
