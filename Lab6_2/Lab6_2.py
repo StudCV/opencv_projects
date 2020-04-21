11872beebc96f76d6e2b041cc116e01bd8635aba
Sergey Son <happinnes_wait@live.com>
2020-04-21 14:36:05 +0300

    Lab6_2

--- Lab6_2/Lab6_2.py
+++ Lab6_2/Lab6_2.py
@@ -4,10 +4,15 @@ import numpy as np
 gray = cv2.imread(r"C:\Users\happi\pics\6_2.png", cv2.IMREAD_REDUCED_GRAYSCALE_2)
 img = cv2.imread(r"C:\Users\happi\pics\6_2.png", cv2.IMREAD_REDUCED_COLOR_2)
 gray = 255 - gray
-lines = cv2.HoughLinesP(gray,1,np.pi/180,212,minLineLength=500,maxLineGap=2)
+#lines = cv2.HoughLinesP(gray,1,np.pi/180,211,minLineLength=500,maxLineGap=2)
+lines = cv2.HoughLines(gray, 0.5, np.pi/180, 400)
 for i in range (np.size(lines, 0)):
-    x1,y1,x2,y2 = lines[i, 0]
-    cv2.line(img,(x1,y1),(x2,y2),(255,255,255))
+    rho, theta = lines[i, 0]
+    x0 = np.cos(theta) * rho
+    y0 = np.sin(theta) * rho
+    pt1 = int(x0 - 1000 * np.sin(theta)), int(y0 + 1000 * np.cos(theta))
+    pt2 = int(x0 + 1000 * np.sin(theta)), int(y0 - 1000 * np.cos(theta))
+    cv2.line(img, pt1, pt2, (255, 255, 255), 2)
 
 cv2.imshow('houghlines3.jpg',img)
 cv2.waitKey(0)
--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-gray = cv2.imread(r"C:\Users\happi\pics\road2.jpg", cv2.IMREAD_GRAYSCALE)
+gray = cv2.imread(r"C:\Users\happi\pics\photo.jpg", cv2.IMREAD_REDUCED_GRAYSCALE_4)
 
-imgScale = 1
-newX,newY = gray.shape[1]*imgScale, gray.shape[0]*imgScale
-gray = cv2.resize(gray,(int(newX),int(newY))) 
+#imgScale = 1
+#newX,newY = gray.shape[1]*imgScale, gray.shape[0]*imgScale
+#gray = cv2.resize(gray,(int(newX),int(newY))) 
 
-binary_thresh, binary=cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+binary_thresh, binary=cv2.threshold(gray, 90, 255, cv2.THRESH_BINARY)
 Otsu_thresh, Otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
 triangle_thresh, triangle = cv2.threshold(gray, 0, 255, cv2.THRESH_TRIANGLE)
-adaptive = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 15)
-gauss = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 3)
+adaptive = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 5)
+gauss = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 10)
 
 
 cv2.imshow('Binary', binary)
